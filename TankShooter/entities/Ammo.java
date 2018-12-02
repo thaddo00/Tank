b618a644d12db9e101e7d1dea7fe677cae2f350d
thaddo00 <43219269+thaddo00@users.noreply.github.com>
2018-12-02 16:17:13 -0600

    Add files via upload

---
+++ TankShooter/entities/Ammo.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class Ammo {
+
+	public int amount;
+	public int total;
+	
+	public Projectile type;
+
+	public Ammo(Projectile projectile, int i) {
+		type = projectile;
+		total = i;
+		amount = i;
+	}
+
+}
